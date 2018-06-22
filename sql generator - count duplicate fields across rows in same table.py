460b976b8c4470febd0d90040f0bf7a17f799851
FELINESOFT\simon.crouch <iamontheinternet@yahoo.com>
2018-06-22 12:43:57 +0100

    added link to user and NO LOCK

--- sql generator - count duplicate fields across rows in same table.py
+++ sql generator - count duplicate fields across rows in same table.py
@@ -7,6 +7,8 @@ def main():
     (
         [Duplicate Count] NVARCHAR(64)
         , [Matched on] NVARCHAR(128)
+        , Link1 NVARCHAR(100)
+        , Link2 NVARCHAR(100)
         , ID bigint
         , DupID bigint
         , DOB date
@@ -20,18 +22,19 @@ def main():
     fields = ['DOB', 'SiteID', 'PostCode', 'FirstName', 'LastName', 'Female', 'PinNumber']
     for length in range(len(fields), 2, -1): # working backwards from most to least combinations
         for combinations in itertools.combinations(fields, length):
-            sql += f"\n\nINSERT INTO @resultants ([Duplicate Count], [Matched on], ID, DupID, DOB, SiteID, PostCode, FirstName, LastName, Female, PinNumber)\nSELECT '{len(combinations)}' 'Duplicate Count', "
+            sql += f"\n\nINSERT INTO @resultants ([Duplicate Count], [Matched on], Link1, Link2, ID, DupID, DOB, SiteID, PostCode, FirstName, LastName, Female, PinNumber)\n" \
+            + f"SELECT '{len(combinations)}' 'Duplicate Count', "
             # Matched on
             matched_on = ''
-            for i, field in enumerate(fields):
+            for _, field in enumerate(fields):
                 # written = False
                 for combination in combinations:
                     if field == combination: # entering if it exists
                         matched_on += (f'{combination}' if len(matched_on) == 0 else f', {combination}')
                         break
             matched_on = matched_on.strip()
-            sql += f"'{matched_on}' 'Matched on', first.ID, second.ID 'DupID', first.DOB, first.SiteID, first.PostCode, first.FirstName, first.LastName, first.Female, first.PinNumber"
-            sql += '\nFROM tblAccount first, tblAccount second WITH(NOLOCK)\nWHERE first.ID < second.ID AND first.Deleted IS NULL AND second.Deleted IS NULL AND \n\tNOT EXISTS (select * from @resultants r WHERE r.ID = first.ID and r.DupID = second.ID)'
+            sql += f"'{matched_on}' 'Matched on', 'http://management.consol.eu/Member/Edit/' + CONVERT(varchar, first.ID), 'http://management.consol.eu/Member/Edit/' + CONVERT(varchar, second.ID), first.ID, second.ID 'DupID', first.DOB, first.SiteID, first.PostCode, first.FirstName, first.LastName, first.Female, first.PinNumber"
+            sql += '\nFROM tblAccount first WITH(NOLOCK), tblAccount second WITH(NOLOCK)\nWHERE first.ID < second.ID AND first.Deleted IS NULL AND second.Deleted IS NULL AND \n\tNOT EXISTS (select * from @resultants r WHERE r.ID = first.ID and r.DupID = second.ID)'
             # WHERE first.field = second.field
             for field in fields:
                 for combination in combinations:
